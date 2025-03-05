4ee03bc7d74a1a77db7f918619b47a8c0e0031fd
JerryChen1031 <jerry_chen1031@icloud.com>
2025-03-05 12:05:13 +0800

    Add files via upload

---
+++ week03-4.c
@@ -0,0 +1,15 @@
+///week03-4.cpp
+///M90H044
+#include <stdio.h>
+int main()
+{
+	int ans = 0;
+	///迴圈前面ans是0
+	int N;///有一個整數N
+	scanf("%d", &N);///讀入 N
+	///人類的迴圈從0開始
+	for(int i=1; i<=N; i++){
+		ans += i;///迴圈中間修改ans
+	}
+	printf("%d", ans);
+}///迴圈後面,印出ans
