f8532f41d276feb6798a8720edf55fdcf860e7a8
JerryChen1031 <jerry_chen1031@icloud.com>
2025-03-12 12:00:36 +0800

    week04

---
+++ week04-2.c
@@ -0,0 +1,10 @@
+///week04-2.c
+int maximumCount(int* nums, int numsSize) {
+    int pos = 0, neg = 0;
+    for(int i=0;i<numsSize; i++){
+        if( nums[i] > 0) pos++;
+        if( nums[i] < 0) neg++;
+    }
+    if(pos>neg) return pos;
+    else return neg;
+}
