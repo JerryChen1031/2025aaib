113a942e99229cbf855f4d7ec935b3ffa57e4720
JerryChen1031 <jerry_chen1031@icloud.com>
2025-05-21 12:02:22 +0800

    week14

---
+++ week14-2.cpp
@@ -0,0 +1,18 @@
+//week14-2.cpp
+#include <stdio.h>
+int main()
+{
+	int a[100];
+	int N, t=1;
+	while(scanf("%d", &N) == 1){
+		for(int i=0; i<N;i++){
+			scanf("%d", &a[i] );
+		}
+
+
+
+
+		printf("Case #%d: It is a B2-Sequence.\n\n", t);
+		t++;
+	}
+}
